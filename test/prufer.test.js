0607e5ac518f60f3e58bf4385bb8ac29af5d2cff
Leon Coto <leoncoto@gmail.com>
2015-07-23 11:51:14 +0100

    Simulating failure.

--- test/prufer.test.js
+++ test/prufer.test.js
@@ -4,7 +4,7 @@ var test = require('tap').test
 
 test('should return an array', function(t) {
   t.ok(_.isArray(prufer([3,3,3,4])), 'prufer returns an array of edges')
-  t.same(prufer([3,3,3,4]), [], 'returns same array', { todo: true })
+  t.same(prufer([3,3,3,4]), [], 'returns same array')
   t.end()
 })
 
