9aaeb87568332fbb73e46fed3e64899709979bef
Leon Coto <leoncoto@gmail.com>
2015-07-24 19:35:48 +0100

    addding one more test and renaming range

--- test/prufer.test.js
+++ test/prufer.test.js
@@ -4,7 +4,11 @@ var test = require('tap').test
 
 test('should return an array', function(t) {
   t.ok(_.isArray(prufer([3,3,3,4])), 'prufer returns an array of edges')
-  t.same(prufer([3,3,3,4]), [], 'returns same array', { todo: true })
+  t.same(
+    prufer([3,3,3,4])
+  , [ [ 3, 0 ], [ 3, 1 ], [ 3, 2 ], [ 4, 3 ], [ 4, 5 ] ]
+  , 'returns same array'
+  )
   t.end()
 })
 
