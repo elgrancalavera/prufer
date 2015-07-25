f4ed39b60287082af72324dbb501a9a0dcbdb7f7
Leon Coto <leoncoto@gmail.com>
2015-07-25 11:38:20 +0100

    Adding file watcher scripts
    - removing lodash

--- test/prufer.test.js
+++ test/prufer.test.js
@@ -1,9 +1,8 @@
 var test = require('tap').test
   , prufer = require('../lib/prufer')
-  , _ = require('lodash')
 
 test('should return an array', function(t) {
-  t.ok(_.isArray(prufer([3,3,3,4])), 'prufer returns an array of edges')
+  t.ok(Array.isArray(prufer([3,3,3,4])), 'prufer returns an array of edges')
   t.same(
     prufer([3,3,3,4])
   , [ [ 3, 0 ], [ 3, 1 ], [ 3, 2 ], [ 4, 3 ], [ 4, 5 ] ]
