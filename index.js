ead9c8c9a559f50c7a36f67cfbbbc49b9c6860c6
Leon Coto <leoncoto@gmail.com>
2015-07-25 16:09:37 +0100

    api and cli sketch

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var prufer = module.exports
+
+prufer.randomSequence = require('./lib/random-prufer-sequence')
+prufer.randomTree = require('./lib/random-tree')
+prufer.treeFromSequence = require('./lib/tree-from-prufer')
