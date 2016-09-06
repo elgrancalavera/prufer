53863b51ac3b55557a5ddf78f1a47275b618f889
Leon Coto <leoncoto@gmail.com>
2016-09-06 20:08:58 +0100

    Add tests for integer array input

--- test/prufer-bin.test.js
+++ test/prufer-bin.test.js
@@ -6,7 +6,7 @@ const testProgram = require('@grancalavera/test-program')
 
 test('prufer bin', t => {
 
-  t.plan(11)
+  t.plan(13)
 
   prufer('--help', {}, function(err, code, stdout, stderr) {
     t.match(stdout, /Usage\: prufer/, 'Should display help message')
@@ -66,6 +66,7 @@ test('prufer bin', t => {
   prufer(['tree', '[3,3,3,4]'], {}, (err, code, stdout, stderr) => {
     const expected = [[3, 0], [3,1], [3, 2], [4, 3], [4, 5]]
         , actual = JSON.parse(stdout)
+
     t.same(actual, expected, 'should produce a known tree')
   })
 
@@ -74,4 +75,13 @@ test('prufer bin', t => {
         , actual = JSON.parse(stdout)
     t.same(actual, expected, 'whitespace should not matter')
   })
+
+  prufer(['tree', '["a", "b", "c"]'], {}, (err, code, stdout, stderr) => {
+    t.notEqual(code, 0, 'expecting error code for non-number arrays')
+  })
+
+  prufer(['tree', '[1.5, 2.5, 3.5, 4.5]'], {}, (err, code, stdout, stderr) => {
+    t.notEqual(code, 0, 'expecting error code for non-integer arrays')
+  })
+
 })
